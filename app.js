b68a6eb4d5b29b83e8d49bcfedcedae4fa7ea08e
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-06-21 21:23:36 +0200

    Added firebase database

---
+++ app.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyCuOaxv2Sk_uTERvGRszehucoiJBYDpGAw",
+  authDomain: "konnect-app-2e773.firebaseapp.com",
+  projectId: "konnect-app-2e773",
+  storageBucket: "konnect-app-2e773.appspot.com",
+  messagingSenderId: "806321298474",
+  appId: "1:806321298474:web:27acd13558d651e8f53753",
+});
+
+const db = firebase.firestore();
